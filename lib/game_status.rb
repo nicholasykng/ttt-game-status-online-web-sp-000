dbdb45da292835432717ef06ea593bdd73a71e02
Nicholas Ng <nicholasykng@gmail.com>
2019-10-13 04:40:02 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,9 +23,9 @@ def won?(board)
       return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
-    else
-      return false
     end
+      return false
+  
   end
 end
 won?(board)
