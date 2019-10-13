608a2b543b177a9c30a1de5854cd1240ba4a82d6
Nicholas Ng <nicholasykng@gmail.com>
2019-10-13 03:26:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,4 +21,5 @@ def won?(board)
     position_1 == position_2 && position_2 == position_3 && position_taken(board, win_index_1)
   end
 end
+won?(board)
 position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
